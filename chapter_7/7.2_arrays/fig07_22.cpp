7ef831b17148bb4d78ea5cfbdb90a0c9976da539
Alair Fonseca <alairfonseca13@gmail.com>
2021-08-15 14:16:36 -0300

    initializing 2d arrays

---
+++ chapter_7/7.2_arrays/fig07_22.cpp
@@ -0,0 +1,32 @@
+/*
+  7.20. 7.22. Initializing multidimensional arrays.
+
+  multidimensional arrays
+*/
+
+#include <iomanip>
+#include <iostream>
+
+#include "../../utils/utils.h"
+
+using std::cout;
+using std::endl;
+using std::setw;
+
+int main() {
+  const int arraySize = 10;
+  int array1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+  int array2[2][3] = {1, 2, 3, 4, 5};
+  int array3[2][3] = {{1, 2}, {4}};
+
+  cout << "Values in array1 by row are:" << endl;
+  printArray(array1, 2, 3);
+
+  cout << "Values in array2 by row are:" << endl;
+  printArray(array2, 2, 3);
+
+  cout << "Values in array3 by row are:" << endl;
+  printArray(array3, 2, 3);
+
+  return 0;
+}
--- utils/utils.h
+++ utils/utils.h
@@ -7,9 +7,23 @@ using std::setw;
 
 template <class T>
 
-void printArray(T array[], T arraySize) {
+void printArray(T array[], int arraySize) {
   for (int i = 0; i < arraySize; i++) {
     cout << setw(4) << array[i];
+  }
+
+  cout << "\n";
+}
+
+template <class T>
+
+void printArray(T array[][3], int rows, int columns) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      cout << setw(4) << array[i][j];
+    }
+
+    cout << endl;
   }
 
   cout << "\n";
